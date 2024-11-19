95ec7934d49f2cdf1f193b411c187b33e89142f9
luanvsalmeida <luanalmeida@alunos.utfpr.edu.br>
2024-11-19 02:02:51 -0300

    feat: Search action page

--- satisfying-you/components/CardOp.tsx
+++ satisfying-you/components/CardOp.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import { StyleSheet, TouchableOpacity, Text} from 'react-native';
 import { theme } from '@/constants/theme';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
@@ -11,7 +11,7 @@ interface Props {
 export function CardOp ({onPress, icon, text}:Props) {
     return (
         <TouchableOpacity style={styles.card} onPress={onPress}>
-            <MaterialCommunityIcons size={75} color="white" style={styles.icon} name={icon}></MaterialCommunityIcons>
+            <MaterialCommunityIcons size={100} color="white" style={styles.icon} name={icon}></MaterialCommunityIcons>
             <Text style={styles.text}>{text}</Text>
         </TouchableOpacity>
        
@@ -22,15 +22,15 @@ const styles = StyleSheet.create({
     card: {
         backgroundColor: theme.colors.darkPurple,
         flexDirection: 'column',
-        height: 150,
-        width: 150,
+        height: 200,
+        width: 200,
         justifyContent: 'center',
         alignItems: 'center'
     },
     text: {
         fontFamily: 'Averia',
         color: theme.colors.white,
-        fontSize: 20
+        fontSize: 24
     },
     icon: {
         flexDirection: 'column',
