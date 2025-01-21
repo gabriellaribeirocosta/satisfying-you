fe240bd1e53347f71e64b40b9b314a51969430d3
Gabriella Costa <gabx2205@gmail.com>
2025-01-21 15:18:32 -0300

    feat: firebase configuration

---
+++ satisfying-you/firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaN25imxz85f1ijSXiS3qmi_mJidiE5b0",
+  authDomain: "satisfying-you-bc1bb.firebaseapp.com",
+  projectId: "satisfying-you-bc1bb",
+  storageBucket: "satisfying-you-bc1bb.firebasestorage.app",
+  messagingSenderId: "551331731904",
+  appId: "1:551331731904:web:f108b7234b8b957999d025"
+};
+
+const app = initializeApp(firebaseConfig);
