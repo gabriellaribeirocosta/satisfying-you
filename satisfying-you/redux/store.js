0777353b54824deff9287189eb651309fe41c3a7
GiDFerreira <giovannaferreira@alunos.utfpr.edu.br>
2025-02-04 16:50:04 -0300

    talvez ajude

--- satisfying-you/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./slices/counterSlice"; //parte do exemplo
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer, //é um exemplo, isso daqui nn tem nada a ver com nossso código
-  },
-});
