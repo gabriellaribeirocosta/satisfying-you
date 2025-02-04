9bac21e965439a3827c80e14934140083248aefb
Gabriella Costa <gabx2205@gmail.com>
2025-02-04 13:52:39 -0300

    Revert "Revert "Crud""

--- satisfying-you/app/screens/Report.tsx
+++ satisfying-you/app/screens/Report.tsx
@@ -1,7 +1,13 @@
 import { StyleSheet, View, Text, Image } from 'react-native';
 import { theme } from '@/constants/theme';
+import { useEffect, useState } from 'react';
+import { collection, doc, onSnapshot, query } from 'firebase/firestore';
+import { db } from '@/firebase/config';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function Report() {
+  const [valor, setValor] = useState([])
+
   const colors = ['#F1CE7E', '#6994FE', '#EA7288', '#5FCDA4', '#53D8D8']; 
 
   const legendItems = [
@@ -11,6 +17,66 @@ export default function Report() {
     { label: 'Ruim', color: colors[3] },
     { label: 'Péssimo', color: colors[4] },
   ];
+
+  const params = useLocalSearchParams();
+  const { id } = params;
+
+  const coletaCollection = collection(db, "nova pesquisa/" + id + "/coletar")
+  //console.log(coletaCollection)
+
+  useEffect(() => {
+    const q = query(coletaCollection)
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const valores = []
+      snapshot.forEach((doc) => {
+        valores.push({
+          id: doc.id,
+          ...doc.data()
+        });
+      })
+      setValor(valores)
+    })
+  }, [])
+
+  //console.log(valor)
+
+  function contarNotas(avaliacoes) {
+    let pessimo = 0;
+    let ruim = 0;
+    let neutro = 0;
+    let bom = 0;
+    let excelente = 0;
+  
+    avaliacoes.forEach((avaliacao) => {
+      const nota = avaliacao.nota;
+  
+      switch (nota) {
+        case 1:
+          pessimo++;
+          break;
+        case 2:
+          ruim++;
+          break;
+        case 3:
+          neutro++;
+          break;
+        case 4:
+          bom++;
+          break;
+        case 5:
+          excelente++;
+          break;
+        default:
+          console.log('Nota inválida');
+          break;
+      }
+    });
+  
+    
+    return [pessimo, ruim, neutro, bom, excelente]
+  }
+
+  console.log(contarNotas(valor))
 
   return (
     <View style={styles.container}>
